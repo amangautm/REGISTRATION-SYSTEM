0526e85df6b9a6936393bf6ad60a377002cc94c3
Aman Gautam <37938543+amangautm@users.noreply.github.com>
2018-10-31 23:33:52 +0530

    Update RegistrationSystem.java

--- RegistrationSystem.java
+++ RegistrationSystem.java
@@ -1,5 +1,26 @@
-package hackerEarth;
-
+import java.util.*;
 public class RegistrationSystem {
-
+	public static void main(String[] args) throws Exception{
+	      Scanner sc =new Scanner(System.in);
+	        HashMap<String,Integer> hm =new HashMap<>();
+	         int n =sc.nextInt();
+	            while( n-->0){
+	                String s =sc.next();
+	                if( !hm.containsKey(s) ){
+	                     hm.put(s,0);
+	                  System.out.println(s);
+	                }
+	                else{
+	                    int temp =hm.get(s);
+	                     String str =s+temp;
+	                      while(hm.containsKey(str)){
+	                          temp++;
+	                          str =s+temp;
+	                      }
+	                      hm.put(str,0);
+	                      System.out.println(str);
+	                      hm.put(s,temp+1);
+	                }
+	            }
+	    }
 }
